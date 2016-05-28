384a3f421c10d2e9d355b42ad6499f0708d86811
ivahaev <ivahaev@gmail.com>
2016-05-29 01:06:39 +0500

    implementing zipping lib and assets and posting to destination

--- src/node-builder.js
+++ src/node-builder.js
@@ -10,6 +10,7 @@ import minimist from "minimist";
 import babelRegister from "babel-register";
 import mkdirp from "mkdirp";
 import merge from "./merge";
+import {EasyZip} from "easy-zip";
 
 let argv = minimist(process.argv.slice(2)),
     help = argv.help || argv.h,
@@ -25,11 +26,11 @@ if (help) {
     console.log("   blank config_path [flags]");
     console.log("");
     console.log("Flags:");
-    console.log("   -o --output string    path or url for config write to");
+    console.log("   -o --output string    path or http server address for config write to");
     console.log("   -w --watch            watch for config changes");
     console.log("");
     console.log("Example:");
-    console.log("   blank ./src -o http://httpbin.org/post -w");
+    console.log("   blank ./src -o http://httpbin.org -w");
     process.exit();
 }
 
@@ -39,19 +40,76 @@ babelRegister({
 });
 
 var usedModules = Object.keys(require.cache);
+let libPath = path.normalize(configPath + "/lib");
+let assetsPath = path.normalize(configPath + "/assets");
 
 console.log(`Building blank from: ${configPath}`);
 prepareConfig();
+zipAndDeliver(libPath, "lib.zip");
+zipAndDeliver(assetsPath, "assets.zip");
+
 if (watch) {
-    let timer = null;
+    let configTimer = null,
+        localTimer = null;
     let configWatcher = chokidar.watch([path.normalize(configPath + path.sep), path.normalize(defaultConfigPath + path.sep)], {
         persistent: true,
         ignoreInitial: true,
-        ignored: [/lib\//, /interfaces\//],
+        ignored: [/lib\//, /interfaces\//, /assets\//],
     });
     configWatcher.on("change", function (path, stats) {
-        clearTimeout(timer);
-        timer = setTimeout(prepareConfig, 500);
+        clearTimeout(configTimer);
+        configTimer = setTimeout(prepareConfig, 500);
+    });
+
+    let localWatcher = chokidar.watch([libPath, assetsPath], {
+        persistent: true,
+        ignoreInitial: true,
+    });
+    localWatcher.on("change", function (path) {
+        clearTimeout(localTimer);
+        if (path.indexOf(assetsPath) === 0) {
+            // upload lib
+            localTimer = setTimeout(() => zipAndDeliver(assetsPath, "assets.zip"), 500);
+            return;
+        }
+        localTimer = setTimeout(() => zipAndDeliver(libPath, "lib.zip"), 500);
+    });
+}
+
+function zipAndDeliver(path, fileName) {
+    let zip = new EasyZip();
+    zip.zipFolder(path, function (err) {
+        if (err) {
+            return console.error(`Can't zip folder ${path}`);
+        }
+        if (output.indexOf("http") === 0) {
+            let localOuput = output + "/" + fileName.replace(".zip", "") + "/" + fileName;
+            console.log(`Posting ${fileName} to service registry: "${localOuput}"`);
+            let srUrl = url.parse(localOuput);
+            let h = srUrl.protocol === "https:" ? https : http;
+            let req = h.request(Object.assign(srUrl, {
+                "method": "POST",
+            }), (res) => {
+                console.log(`Post ${fileName} to service registry result: ${res.statusCode}/${res.statusMessage}`);
+            });
+            req.write(zip.generate({ base64: false, compression: "DEFLATE" }), "binary");
+            req.end();
+            return;
+        }
+        output = path.resolve(output);
+        mkdirp(output, function (e) {
+            if (e) {
+                return console.error(e);
+            }
+            let outputFile = output + path.sep + fileName;
+            console.log(`${new Date()}  Writing ${fileName} to: ${outputFile}`);
+            zip.writeToFile(fileName, function (e) {
+                if (e) {
+                    return console.log(e);
+                }
+                console.log(`${new Date()} The ${fileName} was saved!`);
+            });
+        });
     });
 }
 
@@ -76,8 +134,9 @@ function prepareConfig() {
         return val;
     }, "  ");
     if (output.indexOf("http") === 0) {
-        console.log(`Posting config to service registry: "${output}"`);
-        let srUrl = url.parse(output);
+        let confOutput = output + "/config";
+        console.log(`Posting config to service registry: "${confOutput}"`);
+        let srUrl = url.parse(confOutput);
         let h = srUrl.protocol === "https:" ? https : http;
         let req = h.request(Object.assign(srUrl, {
             "method": "POST",
