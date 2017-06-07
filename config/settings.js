d89d538bd686aee24c8d3436af138ed1c5a37553
Evgeniy Ivakha <ivahaev@gmail.com>
2017-06-07 12:19:34 +0500

    errors updated again

--- config/settings.js
+++ config/settings.js
@@ -230,7 +230,7 @@ module.exports = {
                     emailInvalid: "Некорректный e-mail",
                     emailUsed: "E-mail занят",
                     save: "Ошибка при сохранении изменений",
-                    common: "Произошла ошибка.",
+                    common: "Произошла ошибка:",
                     action: "Ошибка",
                     delete: "Ошибка при удалении",
                     INVALID_OLD_PASSWORD: "Старый пароль не правильный",
