cfd92c24fd186665a5ce3c27e997a14ce1d33c08
Evgeniy Ivakha <ivahaev@gmail.com>
2018-12-11 00:04:25 +0500

    user.dudStart updated

--- config/users.js
+++ config/users.js
@@ -344,9 +344,9 @@ module.exports = {
         },
         storeLifeCycle: {
             didStart: function($db) {
-                console.info("users.didStart task started");
+                console.info("users.didStart task started!!!!!!!!!!!!!!");
                 sync.once("$$usersDidStart", () => {
-                    $db.waitForConnection().then(() => {
+                    $db.waitForConnection("users").then(() => {
                         console.info("Checking root user in DB...");
                         const fn = async () => {
                             try {
